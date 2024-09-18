9482adc4e5dd365cb113220512201e5922f8032c
Wesley Heberling <wesleyheberling@yahoo.com>
2024-09-18 16:16:31 -0700

    Removed Vercel deployment files

--- Vercel.js
+++
@@ -1,4 +0,0 @@
-PK    IYÌf+šk   Æ   
-   Vercel.txt«æåRPP*K-*ÎÌÏS²R0Ò
-$•fæ¤ùÑ ®‚B5„Ê%…•ŠS‹€šô²Š•tàR¥Å© )  DrjŽ~^~JªD²DÅBŒ.Ê/-IÅg´¾†ž–&’±)©Å%¨V"ÊË¤PK     IYÌf+šk   Æ   
-                Vercel.txtPK      8   “     
